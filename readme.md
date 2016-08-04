c069884f70a31f24e8b1deb7771369859d68298a
Brice Colucci <brice.colucci@gmail.com>
2016-08-04 11:27:46 +0200

    wip

--- readme.md
+++ readme.md
@@ -1,14 +1,14 @@
 
 # River.js
 
-**in active development**
+**In active development**, feel free to contribute.
 
 ## Purpose
 
 River goal is to provide a very simple way to create (work)flows of data (streams).
 River is not another stream library but something to help you to work with streams eyes closed...
 
-[Read the documentation](https://github.com/wuha-io/river/blob/master/docs/)
+[Generated documentation](https://github.com/wuha-io/river/blob/master/docs/)
 
 # Test it
 
@@ -20,15 +20,10 @@ River is not another stream library but something to help you to work with strea
     # run an example
     ./bnode examples/users.js
 
-## Examples
-
-- [src/examples/users.js](https://github.com/wuha-io/river/blob/master/src/examples/users.js)
-- [src/examples/events.js](https://github.com/wuha-io/river/blob/master/src/examples/events.js)
-
-## Todo
+## Roadmap
 
   - Implement all the tests
-  - Documentate the code
+  - Make documentation
   - Implement sources (Elasticsearch, File, JSON, SQLite...)
   - Implement some endpoints
   - Make a string grammar we can compile to JavaScript with River
---
+++ test/processes/cast.js
@@ -0,0 +1,51 @@
+
+import { ok, deepEqual } from 'assert'
+import { cast } from '../../src/processes'
+import createStream from '../../src/streams/stream'
+
+describe('processes/cast', () => {
+
+    it('cast elements from a stream (simple type)', (done) => {
+      const stream = createStream()
+      const received = []
+      cast({ type: Number })(stream)
+        .on('data', received.push.bind(received))
+        .on('end', () => {
+          ok(isNaN(received.shift()))
+          deepEqual(received, [ 1, 2.6 ])
+          done()
+        })
+      stream.write('a')
+      stream.write('1')
+      stream.write('2.6')
+      stream.emit('end')
+    })
+
+    it('cast elements from a stream (custom type)', (done) => {
+
+      class MyCustomType {
+        constructor(x) {
+          const n = Number(x)
+          this.value = isNaN(n) ? null : n
+        }
+      }
+      
+      const stream = createStream()
+      const received = []
+      cast({ type: 'MyType', definition: MyCustomType })(stream)
+        .on('data', received.push.bind(received))
+        .on('end', () => {
+          deepEqual(received, [
+            new MyCustomType(),
+            new MyCustomType(1),
+            new MyCustomType(2.6)
+          ])
+          done()
+        })
+      stream.write('a')
+      stream.write('1')
+      stream.write('2.6')
+      stream.emit('end')
+    })
+
+})
