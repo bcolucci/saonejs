f3f6a4a8a73ec7674fc0487449fb0b4d5b5cd23c
Brice Colucci <brice.colucci@gmail.com>
2016-08-04 17:25:30 +0200

    wip

--- bin/processesList.sh
+++ bin/processesList.sh
@@ -2,5 +2,6 @@
 
 # generate the markdown processes list
 for f in $(find docs/processes -name '*.md' -type f); do
-  echo "* [$(basename $f)](https://github.com/wuha-io/river/blob/master/$f)"
+  fname=$(basename $f .md)
+  echo "* $fname [doc](https://github.com/wuha-io/river/blob/master/$f) | [source](https://github.com/wuha-io/river/blob/master/src/processes/$fname.js)"
 done
