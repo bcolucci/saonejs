cff6d5143fea800a9d96f5bf81c7f7a1f9dff410
Brice Colucci <brice.colucci@gmail.com>
2016-08-05 13:52:04 +0200

    wip

--- test/processes/compact.js
+++ test/processes/compact.js
@@ -6,21 +6,21 @@ import createStream from '../../src/streams/stream'
 
 describe('processes/compact', () => {
 
-  // TODO fix the process
-  /*it('...............................', done => {
+  //TODO the description
+  it('*****************************************', done => {
     const stream = createStream()
     const received = []
     compact({ test: (cur, prev, buffer) => !prev || equals(cur, prev), compact: last })(stream)
       .on('data', received.push.bind(received))
       .on('end', () => {
-        console.log('input', 'aabcdddeffg')
-        console.log('received', received.join(''))
-        deepEqual(received, 'abcddeffg'.split(''))
+        //console.log('received', received.join(''))
+        //TODO the assertion
+          //deepEqual(received, 'abcddeffg'.split(''))
         done()
       })
-    'aabcdddeffg'.split('')
+    'aabcddddddeffg'.split('')
       .forEach(n => stream.write(n))
     stream.emit('end')
-  })*/
+  })
 
 })
